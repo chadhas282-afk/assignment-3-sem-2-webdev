ce0c8b6744579c5869fed43b4de8b11fda859ae8
chadhas282-afk <chadhas282@gmail.com>
2026-04-12 11:30:13 +0530

    Update package.json and package-lock.json to reflect new React version; modify main.jsx for proper rendering with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
- <React.StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
