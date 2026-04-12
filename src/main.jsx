e929c7e2873d8c98d51effe2abd4d1be5afaa513
chadhas282-afk <chadhas282@gmail.com>
2026-04-12 11:15:17 +0530

    Refactor package.json and package-lock.json to update app name and add react-router-dom dependency; modify main.jsx to wrap App component with HashRouter and React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
-
+import './index.css'
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
-  <App />
-</HashRouter>,
+ <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 )
