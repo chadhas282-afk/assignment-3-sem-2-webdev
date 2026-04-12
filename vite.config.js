debbdda360a687db8196a0393823bbcbe1a200ac
chadhas282-afk <chadhas282@gmail.com>
2026-04-12 11:13:22 +0530

    Update base path in vite.config.js for deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Report-Card/',
+  base: '/assignment-3-sem-2-webdev/',
 })
